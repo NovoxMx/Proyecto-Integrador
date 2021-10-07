f908eb035a8961bfb2edea6753803555eebe09f4
Marco <mctulio.2@gmail.com>
2021-10-06 18:18:35 -0700

    funcion 1 change

--- Funciones/Funcion_1.py
+++ Funciones/Funcion_1.py
@@ -1,1 +1,3 @@
+#función de inventario en forma de lista con: tipo de artículo y cantidad, marcar cuando hay menos de 5 en existencia.
+#función por: Marco Tulio Montoya Angulo - A01254155
 print('test')
