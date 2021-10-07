3f63f5469a71b4e56262f41e89756fe43164103a
Marco <mctulio.2@gmail.com>
2021-10-06 18:31:34 -0700

    funcion 1 base

--- Funciones/Funcion_1.py
+++ Funciones/Funcion_1.py
@@ -1,3 +1,15 @@
 #función de inventario en forma de lista con: tipo de artículo y cantidad, marcar cuando hay menos de 5 en existencia.
 #función por: Marco Tulio Montoya Angulo - A01254155
-print('test')
+#def almacenamiento ():
+print('Bienvenido al inventario, seleccione una opción: ')
+print ('"1" Si desea ver el inventario de dispositivos.')
+print ('"2" Si desea ver el inventario de otros.')
+opcion = int(input())
+
+if opcion < 1 or opcion >2:
+    print ('Ingrese un número valido.')
+    
+elif opcion == 1:
+    print ('Inventario 1')
+elif opcion == 2:
+    print('Inventario 2')
