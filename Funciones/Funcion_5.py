053bc70ed144de3f186fbda54a8f62e84f4f8d10
Andre Castillo <a01254180@itesm.mx>
2021-10-07 12:29:15 -0700

    Principio funcion_5

--- Funciones/Funcion_5.py
+++ Funciones/Funcion_5.py
@@ -1,2 +1,8 @@
 #Agregar productos a almacén
 #André Castillo ya la apartó ingatu
+
+def agregar_producto(producto, cantidad):
+    a
+
+p = int(input("¿Qué producto desea agregar?"))
+#print productos
