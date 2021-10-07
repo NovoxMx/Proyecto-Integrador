0d21c27d62c9482518cb8173acfac1add6320d42
Andre Castillo <a01254180@itesm.mx>
2021-10-07 12:49:32 -0700

    Avance funcion_5

--- Funciones/Funcion_5.py
+++ Funciones/Funcion_5.py
@@ -2,7 +2,25 @@
 #André Castillo ya la apartó ingatu
 
 def agregar_producto(producto, cantidad):
-    a
+    almacen_viejo = open("almacenaje.txt", "r")
+    lista_almacen = almacen_viejo.readlines()
+    almacen_viejo.close()
+    print(lista_almacen)
+    almacen_nuevo = open("almacenaje.txt", "w")
+
+    almacen_nuevo.close()
+    
+
+#display productos
+#Ejemplo_1:
+print("[1] - Iphone 11")
 
 p = int(input("¿Qué producto desea agregar?"))
-#print productos
+#Asignación número-producto
+#Ejemplo_1:
+if p == 1:
+    Producto = "Iphone 11"
+
+Cantidad = int(input("¿Qué cantidad de productos desea agregar?"))
+
+agregar_producto(Producto, Cantidad)
