0d8708fe4dab1b29af30a49bf4cdbacda5e00426
Andre Castillo <a01254180@itesm.mx>
2021-10-08 08:05:18 -0700

    Ejemplo Funcion_5

--- Funciones/Funcion_5.py
+++ Funciones/Funcion_5.py
@@ -6,21 +6,22 @@ def agregar_producto(producto, cantidad):
     lista_almacen = almacen_viejo.readlines()
     almacen_viejo.close()
     print(lista_almacen)
-    almacen_nuevo = open("almacenaje.txt", "w")
+    print(lista_almacen[producto * 2])
+    #almacen_nuevo = open("almacenaje.txt", "w")
 
-    almacen_nuevo.close()
+    #almacen_nuevo.close()
     
 
 #display productos
 #Ejemplo_1:
 print("[1] - Iphone 11")
 
-p = int(input("¿Qué producto desea agregar?"))
+p = int(input("¿Qué producto desea agregar? "))
 #Asignación número-producto
 #Ejemplo_1:
 if p == 1:
     Producto = "Iphone 11"
 
-Cantidad = int(input("¿Qué cantidad de productos desea agregar?"))
+Cantidad = int(input("¿Qué cantidad de productos desea agregar? "))
 
-agregar_producto(Producto, Cantidad)
+agregar_producto(p, Cantidad)
