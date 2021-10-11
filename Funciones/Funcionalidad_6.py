0731b8e56c05732b2c3c297118f71dda5933e905
NovoxMx <A00835132@tec.mx>
2021-10-11 03:42:18 -0700

    alv

---
+++ Funciones/Funcionalidad_6.py
@@ -0,0 +1,39 @@
+#Empleados y agreagar a empleados
+#Por Angel Marin
+import csv
+
+print("Empleados")
+print("Que quiere hacer: \n[1]Agregar Nuevo empleado. \n[2]Mostrar a los empleados.")
+valor= int(input("Elegir: "))
+
+if valor == 1:
+    archivo= open("Proyecto-Integrador\Funciones\Empleados.csv", "a")
+    #Esa es mi direccion al archivo, si lo quieren cambiar haganlo
+    nombre= input("Nombre del empleado: ")
+    apellido= input("Apellido del empleado: ")
+    edad= input("Introduce la edad: ")
+    horario= input("Horario de trabajo: ")
+    dia_Descanso= input("Dia de Descanso: ")
+
+    archivo.write(apellido)
+    archivo.write(",")
+    archivo.write(nombre)
+    archivo.write(",")
+    archivo.write(edad)
+    archivo.write(",")
+    archivo.write(horario)
+    archivo.write(",")
+    archivo.write(dia_Descanso)
+    archivo.write("\n")
+    archivo.close()
+
+elif valor == 2:
+    print("Empleados: \n")
+
+    with open("Proyecto-Integrador\Funciones\Empleados.csv", newline= '') as csvfile:
+        archivo1=  csv.reader(csvfile, delimiter=',')
+
+        for filas in archivo1:
+            print (filas)
+
+
