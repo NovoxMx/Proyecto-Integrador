10d86efa06944a9deb0a4e80183b01299bea56ce
Kmaldon <A01252830@itesm.mx>
2021-10-11 18:07:53 -0700

    haha

--- Funciones/Funcionalidad_2.py
+++ Funciones/Funcionalidad_2.py
@@ -1,54 +1,57 @@
+#Información de los productos
+#Juan Carlos Garcia A01252830
+
 def samsung(telefono):
     if telefono == 1:
-        print('\nGalaxy Note 20 Ultra \nMemoria ram = 12 GB RAM LPDDR5 \nCapacidad de almacenamiento = 256/512 (con MicroSD\nPantalla = Dynamic AMOLED 6,9" 3.088 x 1.440 px WQHD+')
+        print('\nGalaxy Note 20 Ultra \nFecha de lanzamiento = 21 de agosto de 2020 \nMemoria = 512GB 12GB RAM \nPantalla = 116.7 cm2 Dynamic AMOLED 2X \nResolución = 1440 x 3088 píxeles')
     elif telefono  == 2:
-        print("\nGalaxy Z Fold3 5G \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nGalaxy Z Fold3 5G \nFecha de lanzamiento = 27 de agosto de 2021 \nMemoria = 512GB 12GB RAM \nPantalla = 179.9 cm2 Foldable Dynamic AMOLED 2X\nResolución = 1768 x 2208 píxeles")
     else:
-        print("\nSamsung Galaxy A32 \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nSamsung Galaxy A32 \nFecha de lanzamiento = 25 de febrero de 2021 \nMemoria = 128GB 8GB RAM \nPantalla = 98.9 cm2 Super AMOLED \nResolución = 1080 x 2400 píxeles")
         
 
 def iphone(telefono):
     if telefono == 1:
-        print("\niPhone 12 \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\niPhone 12 \nFecha de lanzamiento = 23 de octubre de 2020 \nMemoria = 256GB 4GB RAM \nPantalla = 90.2 cm2 Super Retina XDR OLED \nResolución = 1170 x 2532 píxeles")
     elif telefono  == 2:
-        print("\niPhone 11 Pro Max \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\niPhone 11 Pro Max \nFecha de lanzamiento = 20 de septiembre de 2019 \nMemoria = 512GB 4GB RAM \nPantalla = 102.9 cm2 Super Retina XDR OLED \nResolución = 1242 x 2688 píxeles")
     else:
-        print("\niPhone Xr \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\niPhone Xr \nFecha de lanzamiento = 12 de septiembre de 2018 \nMemoria = 256GB 3GB RAM \nPantalla = 90.3 cm2 Liquid Retina IPS LCD \nResolución = 828 x 1792 píxeles")
         
 
 def huawei(telefono):
     if telefono == 1:
-        print("\nHuawei P40 Pro \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nHuawei P40 Pro \nFecha de lanzamiento = 07 de abril de 2020 \nMemoria = 512GB 8GB RAM \nPantalla = 105.2 cm2 OLED \nResolución = 1200 x 2640 píxeles")
     elif telefono  == 2:
-        print("\nHuawei Mate 40 PRO \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nHuawei Mate 40 PRO \nFecha de lanzamiento = 22 de octubre de 2020 \nMemoria = 512GB 8GB RAM \nPantalla = 115.7 cm2 OLED \nResolución = 1344 x 2772píxeles")
     else:
-        print("\nHuawei P30 lite \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nHuawei P30 lite \nFecha de lanzamiento = 27 de marzo de 2019 \nMemoria = 256GB 6GB RAM \nPantalla = 93.6 cm2 IPS LCD \nResolución = 1080 x 2312 píxeles")
         
 
 def xiaomi(telefono):
     if telefono == 1:
-        print("\nXiaomi Redmi Note 10 Pro \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nXiaomi Redmi Note 10 Pro \nFecha de lanzamiento = 04 de marzo de 2021 \nMemoria = 128GB 8GB RAM \nPantalla = 107.4 cm2 AMOLED \nResolución = 1080 x 2400 píxeles")
     elif telefono  == 2:
-        print("\nXiaomi Mi Mix 2S \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nXiaomi Mi Mix 2S \nFecha de lanzamiento = 26 de Abril de 2018\nMemoria = 256GB 8GB RAM \nPantalla = 92.6 cm2 IPS LCD \nResolución = 1080 x 2160 píxeles")
     else:
-        print("\nXiaomi Mi 11 lite \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nXiaomi Mi 11 lite \nFecha de lanzamiento = 29 de marzo de 2021 \nMemoria = 128GB 8GB RAM \nPantalla = 103.6 cm2 AMOLED \nResolución = 1080 x 2400 píxeles")
         
 
 def motorola(telefono):
     if telefono == 1:
-        print("\nMotorola Moto G60 \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nMotorola Moto G60 \nFecha de lanzamiento = 27 de abril de 2021 \nMemoria = 128GB 6GB RAM \nPantalla = 109.8 cm2 IPS LCD \nResolución = 1080 x 2460 píxeles")
     elif telefono  == 2:
-        print("\nMotorola Edge 20 \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nMotorola Edge 20 \nFecha de lanzamiento = 29 de julio de 2021 \nMemoria = 256GB 8GB RAM \nPantalla = 110.2 cm2 OLED \nResolución = 1080 x 2400 píxeles")
     else:
-        print("\nMotorola XT2125-4 \nMemoria ram = \nCapacidad de almacenamiento = \nPantalla = ")
+        print("\nMotorola XT2125-4 \nFecha de lanzamiento = 25 de marzo de 2021 \nMemoria = 256GB 12GB RAM \nPantalla = 104.9 cm2 IPS LCD \nResolución = 1080 x 2520 píxeles")
         
 
-    
-print("1-Samsung, 2-Iphone, 3-Huawei, 4-Xiaomi, 5-motorola")
-marca = int(input("Ingresa el numero de la marca: "))
+
+print("1-Samsung \n2-Iphone \n3-Huawei \n4-Xiaomi \n5-Motorola \n6-Accesorios")
+marca = int(input("Ingresa el numero de la marca o los accesorios: "))
 
 if marca == 1:
-    print("\n1-Galaxy Note 20 Ultra, 2-Galaxy Z Fold3 5G, 3-Samsung Galaxy A3")
+    print("\n1-Galaxy Note 20 Ultra \n2-Galaxy Z Fold3 5G \n3-Samsung Galaxy A3")
     phone = int(input("Ingresa el numero del telefono: "))
     if phone >= 1 and phone <= 3:
         samsung(phone)
@@ -56,7 +59,7 @@ if marca == 1:
         print("Numero invalido")
         
 elif marca == 2:
-    print("\n1-iPhone 12, 2-iPhone 11 Pro Max, 3-iPhone Xr")
+    print("\n1-iPhone 12 \n2-iPhone 11 Pro Max \n3-iPhone Xr")
     phone = int(input("Ingresa el numero del telefono: "))
     if phone >= 1 and phone <= 3:
         iphone(phone)
@@ -64,7 +67,7 @@ elif marca == 2:
         print("Numero invalido")
     
 elif marca == 3:
-    print("\n1-Huawei P40 Pro, 2-Huawei Mate 40 PRO, 3-Huawei P30 lite")
+    print("\n1-Huawei P40 Pro \n2-Huawei Mate 40 PRO \n3-Huawei P30 lite")
     phone = int(input("Ingresa el numero del telefono: "))
     if phone >= 1 and phone <= 3:
         huawei(phone)
@@ -72,7 +75,7 @@ elif marca == 3:
         print("Numero invalido")
     
 elif marca == 4:
-    print("\n1-Xiaomi Redmi Note 10 Pro, 2-Xiaomi Mi Mix 2S, 3-Xiaomi Mi 11 lit")
+    print("\n1-Xiaomi Redmi Note 10 Pro \n2-Xiaomi Mi Mix 2S \n3-Xiaomi Mi 11 lit")
     phone = int(input("Ingresa el numero del telefono: "))
     if phone >= 1 and phone <= 3:
         xiaomi(phone)
@@ -80,12 +83,15 @@ elif marca == 4:
         print("Numero invalido")
     
 elif marca == 5:
-    print("\n1-Motorola Moto G60, 2-Motorola Edge 20, 3-Motorola XT2125-4")
+    print("\n1-Motorola Moto G60 \n2-Motorola Edge 20 \n3-Motorola XT2125-4")
     phone = int(input("Ingresa el numero del telefono: "))
     if phone >= 1 and phone <= 3:
         motorola(phone)
     else:
         print("Numero invalido")
+
+elif marca == 6:
+    print("\nAccesorios: \nCargador Tipo C \nCargador iPhone \nCargador Micro Usb \nAudifonos Bluetooth \nAirpod \nFundas \nProtector de pantalla \nTarjeta de memoria (64Gb) \nBateria portatil \nPopsockets")
     
 else:
     print("Numero invalido")
